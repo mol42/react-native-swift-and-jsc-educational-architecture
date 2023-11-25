1e9a6267ae9eb5a2b9b609a1dbcd8cbd8b2a8e5b
Tayfun YUGRUK <tayfunyugruk@gmail.com>
2023-11-25 03:15:51 -0500

    Initial Commit

---
+++ ReactNativeBasics/ReactNativeBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactNativeBasicsApp.swift
+//  ReactNativeBasics
+//
+//  Created by Typhoon Y on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ReactNativeBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
