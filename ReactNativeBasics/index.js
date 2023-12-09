e6188d2f5dab0a51821e9642c5f6ff1bc36219fe
Tayfun YUGRUK <tayfunyugruk@gmail.com>
2023-12-09 16:32:58 -0500

    simple react changes applied

--- ReactNativeBasics/index.js
+++ ReactNativeBasics/index.js
@@ -19,170 +19,183 @@ var console = {
 
 /*****************************************************************************************************/
 
-/**  simple.react.js START */
+/**  simple-react.js START */
 const ReactInnerContext = {
-    elementId: 0,
-    activeId: null,
-    stateMap: {},
-    hookIdMap: {},
-    activeTreeNode: null,
-    virtualDomTree: null
+  elementId: 0,
+  activeId: null,
+  stateMap: {},
+  hookIdMap: {},
+  virtualDomTree: null,
+  activeStateContext: null
 };
 
 const Fragment = "fragment";
 
 function resetReactContext() {
-    ReactInnerContext.elementId = 0;
-    ReactInnerContext.hookIdMap = {};
-    ReactInnerContext.activeStateParent = null;
+  ReactInnerContext.elementId = 0;
+  ReactInnerContext.hookIdMap = {};
+  ReactInnerContext.activeStateContext = null;
 }
 
 function requestReRender(elementId) {
-    resetReactContext();
-    const existingDomTree = ReactInnerContext.virtualDomTree;
-    // our framework expects function that creates the
-    // virtual dom tree
-    const newVirtualDomTree = createElement(existingDomTree.type);
-    renderVirtualDom(newVirtualDomTree, ReactInnerContext.rootDOMElement, true);
+  resetReactContext();
+  const existingDomTree = ReactInnerContext.virtualDomTree;
+  // our framework expects function that creates the
+  // virtual dom tree
+  const newVirtualDomTree = createElement(existingDomTree.type);
+  renderVirtualDom(newVirtualDomTree, ReactInnerContext.rootDOMElement, true);
 }
 
 function createOrGetMap(map, activeElementId) {
-    const resultArray = [];
+  const resultArray = [];
 
-    if (typeof map[activeElementId] === "undefined") {
-        map[activeElementId] = {};
-        resultArray.push(false);
-    } else {
-        resultArray.push(true);
-    }
+  if (typeof map[activeElementId] === "undefined") {
+    map[activeElementId] = {};
+    resultArray.push(false);
+  } else {
+    resultArray.push(true);
+  }
 
-    resultArray.push(map[activeElementId]);
+  resultArray.push(map[activeElementId]);
 
-    return resultArray;
+  return resultArray;
 }
 
+// simple useState hook
+// NOTE: Not the real implementation, educational implementation
 function useState(initialState) {
-    const { $$id, $$parentId } = ReactInnerContext.activeStateParent;
-    const { hookIdMap, stateMap } = ReactInnerContext;
-    const activeStateId = $$parentId || "NULL";
-    const [hookIdMapAlreadyCreated, activeHookIdMap] = createOrGetMap(hookIdMap, activeStateId);
-    const [activeStateMapAlreadyCreated, activeStateMap] = createOrGetMap(stateMap, activeStateId);
-    
-    if (!hookIdMapAlreadyCreated) {
-        activeHookIdMap["id"] = 0;
-    }
-    
-    const activeHookId = activeHookIdMap["id"]++;
-    
-    if (!activeStateMapAlreadyCreated && !hookIdMapAlreadyCreated) {
-        activeStateMap[activeHookId] = initialState;
-    } else {
-        if (typeof activeStateMap[activeHookId] === "undefined") {
-            activeStateMap[activeHookId] = initialState;
-        }
-    }
+  // Since JS is single thread we have the ability to reach a global object
+  // safely while createElement is executing a React component function.
+  // useState and other hooks are only meant to be used inside the
+  // component functions so accessing the ReactInnerContext.activeStateContext
+  // enables us to create id values for each hook function and help on
+  // their data management
+  const { $$id, $$parentId } = ReactInnerContext.activeStateContext;
+  const { hookIdMap, stateMap } = ReactInnerContext;
+  const activeStateId = $$parentId || "NULL";
+  const [hookIdMapAlreadyCreated, activeHookIdMap] = createOrGetMap(hookIdMap, activeStateId);
+  const [activeStateMapAlreadyCreated, activeStateMap] = createOrGetMap(stateMap, activeStateId);
 
-    const stateUpdater = function(newState) {
-        activeStateMap[activeHookId] = newState;
-        requestReRender(activeStateId);
-        setTimeout(function() {
-            requestReRender($$parentId, $$id);
-        }, 20);
-    };
+  if (!hookIdMapAlreadyCreated) {
+    activeHookIdMap["id"] = 0;
+  }
 
-    return [activeStateMap[activeHookId], stateUpdater];
+  const activeHookId = activeHookIdMap["id"]++;
+
+  if (!activeStateMapAlreadyCreated && !hookIdMapAlreadyCreated) {
+    activeStateMap[activeHookId] = initialState;
+  } else {
+    if (typeof activeStateMap[activeHookId] === "undefined") {
+      activeStateMap[activeHookId] = initialState;
+    }
+  }
+
+  const stateUpdater = function (newState) {
+    activeStateMap[activeHookId] = newState;
+    requestReRender(activeStateId);
+    setTimeout(function () {
+      requestReRender($$parentId, $$id);
+    }, 20);
+  };
+
+  return [activeStateMap[activeHookId], stateUpdater];
 }
 
 function createElement(nodeTypeOrFunction, props, ...children) {
-    let treeNode = {
-        $$id: `element-${ReactInnerContext.elementId++}`,
-        $$parentId: null,
-        type: nodeTypeOrFunction,
-        props: props || {},
-        children: null,
-        $$nativeElement: null // will be filled later
-    };
+  let treeNode = {
+    $$id: `element-${ReactInnerContext.elementId++}`,
+    $$parentId: null,
+    type: nodeTypeOrFunction,
+    props: props || {},
+    children: null,
+    $$nativeElement: null // will be filled later
+  };
 
-    if (typeof nodeTypeOrFunction === "function") {
-        // thanks to single thread abilitiy of JS we can create
-        // id values for the hooks to use
-        ReactInnerContext.activeStateParent = treeNode;
-        treeNode.children = [nodeTypeOrFunction(props, children)];
-        applyParentToChildren(treeNode.children, treeNode.$$id);
+  if (typeof nodeTypeOrFunction === "function") {
+    // thanks to single thread abilitiy of JS we can create
+    // id values for the hooks to use by accessing the global
+    // ReactInnerContext.activeStateContext in useState or any
+    // other built-in hook
+    ReactInnerContext.activeStateContext = treeNode;
+    treeNode.children = [nodeTypeOrFunction(props, children)];
+    applyParentToChildren(treeNode.children, treeNode.$$id);
+  } else {
+    if (children != null && children.length === 1 && typeof children[0] === "string") {
+      treeNode.props.__innerHTML = children[0];
     } else {
-        if (children != null && children.length === 1 && typeof children[0] === "string") {
-            treeNode.props.__innerHTML = children[0];
-        } else {
-            if (children != null && children.length > 0) {
-                treeNode.children = children;
-                applyParentToChildren(treeNode.children, treeNode.$$id);
-            }
-        }
+      if (children != null && children.length > 0) {
+        treeNode.children = children;
+        applyParentToChildren(treeNode.children, treeNode.$$id);
+      }
     }
+  }
 
-    return treeNode;
+  return treeNode;
 }
 
 function applyParentToChildren(children, parentId) {
-    if (children != null && children.length > 0) {
-        children.forEach(child => {
-            if (child != null) {
-                child.$$parentId = parentId;
-            }
-        });
-    }
+  if (children != null && children.length > 0) {
+    children.forEach((child) => {
+      if (child != null) {
+        child.$$parentId = parentId;
+      }
+    });
+  }
 }
 
 function renderVirtualDom(virtualDomTree, rootDOMElement, replacePreviousRoot) {
-    ReactInnerContext.virtualDomTree = virtualDomTree;
-    ReactInnerContext.rootDOMElement = rootDOMElement;
-    ReactInnerContext.rootRenderer(virtualDomTree, rootDOMElement, replacePreviousRoot);
+  ReactInnerContext.virtualDomTree = virtualDomTree;
+  ReactInnerContext.rootDOMElement = rootDOMElement;
+  ReactInnerContext.rootRenderer(virtualDomTree, rootDOMElement, replacePreviousRoot);
 }
 
 function createRoot(rootDOMElement) {
-    return {
-        render: function(virtualDomTree) {
-            resetReactContext();
-            renderVirtualDom(virtualDomTree, rootDOMElement);
-        }
-    };
+  return {
+    render: function (virtualDomTree) {
+      resetReactContext();
+      renderVirtualDom(virtualDomTree, rootDOMElement);
+    }
+  };
 }
 
 function findAndInvokeEventHandlerOfElement(elementNodeInVirtualDomTree, elementId, eventKey, evt) {
-    const elemNode = elementNodeInVirtualDomTree;
-    
-    if (elemNode === null) {
-        return;
+  const elemNode = elementNodeInVirtualDomTree;
+
+  if (elemNode === null) {
+    return;
+  }
+
+  if (elemNode.$$id === elementId) {
+    elemNode.props?.events[eventKey]?.(evt);
+  } else {
+    if (elemNode.children) {
+      if (Array.isArray(elemNode.children)) {
+        elemNode.children.forEach((singleElement) => {
+          findAndInvokeEventHandlerOfElement(singleElement, elementId, eventKey, evt);
+        });
+      } else {
+        findAndInvokeEventHandlerOfElement(elemNode.children, elementId, eventKey, evt);
+      }
     }
-    
-    if (elemNode.$$id === elementId) {
-        elemNode.props?.events[eventKey]?.(evt);
-    } else {
-        if (elemNode.children) {
-            if (Array.isArray(elemNode.children)) {
-                elemNode.children.forEach((singleElement) => {
-                    findAndInvokeEventHandlerOfElement(singleElement, elementId, eventKey, evt);
-                });
-            } else {
-                findAndInvokeEventHandlerOfElement(elemNode.children, elementId, eventKey, evt);
-            }
-        }
-    }
+  }
 }
 
 /**
- * BELOW 2 METHODS ARE USED FOR GLOBAL PURPOSES
+ * BELOW 2 METHODS ARE USED FOR COMBINING TWO REALMS
  */
+
+// This method is used by the renderer system to inform React side for an event
 function __informNativeEvent(elementId, eventKey, evt) {
-    const {
-        virtualDomTree
-    } = ReactInnerContext;
-    findAndInvokeEventHandlerOfElement(virtualDomTree, elementId, eventKey, evt);
+  const { virtualDomTree } = ReactInnerContext;
+  findAndInvokeEventHandlerOfElement(virtualDomTree, elementId, eventKey, evt);
 }
 
+// This method is used for registering the renderer function
 function __registerRootRenderer(rootRenderer) {
-    ReactInnerContext.rootRenderer = rootRenderer;
+  ReactInnerContext.rootRenderer = rootRenderer;
 }
+
+console.log("Modular React is initialized");
 
 /**  simple.react.js END */
 /*****************************************************************************************************/
